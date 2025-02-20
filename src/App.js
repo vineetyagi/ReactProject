6b9c3a54b54cc027c733fe7853d6b2954030d071
vineet <mrtyagi123@gmail.com>
2025-02-20 11:39:10 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import{useState} from 'react';
 
 function App() {
+  let[status,setStatus]=useState(false)
+  let[pstatus,setPstatus]=useState(false)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={()=>setStatus(!status)}>
+        {
+          (status)
+          ?
+          'Hide'
+          :
+          'Show'
+        }
+      </button>
+      <br/>
+      {
+        (status)
+        ?
+        <div>
+        <input type={(pstatus)?'text':'password'} />
+        <br/>
+        <button onClick={()=>setPstatus(!pstatus)}>{(pstatus)?'Hide Password':'Show Password'}</button>
+        </div>
+        :
+        <p>Click on the button</p>
+      }
     </div>
   );
 }
