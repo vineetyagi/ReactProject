76b6028914c53f2fa22bb91c3b19394bd3f515b2
vineet <mrtyagi123@gmail.com>
2025-02-20 22:11:52 +0530

    new data

--- src/App.js
+++ src/App.js
@@ -4,8 +4,20 @@ import{useState} from 'react';
 function App() {
   let[status,setStatus]=useState(false)
   let[pstatus,setPstatus]=useState(false)
+  let[menueStatus,setMenueStatus]=useState(false)
   return (
     <div className="App">
+      <button className={'micon'} onClick={()=>setMenueStatus(!menueStatus)}>
+        {menueStatus?<span>&times;</span>:<span>&#9776;</span>}
+        </button>
+      <div className={`menue ${menueStatus?'activeMenue':''}`}>
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Services</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
       <button onClick={()=>setStatus(!status)}>
         {
           (status)
